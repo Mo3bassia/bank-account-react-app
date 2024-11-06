987287eb55d1d5a2900644c0b84dc21aa847a985
Mohamed Abassia <mo3bassia@gmail.com>
2024-11-06 16:58:32 +0200

    add footer Component and some styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import Mo3bassia from "./Mo3bassia";
 
 const initialState = {
   balance: 0,
@@ -32,75 +33,78 @@ export default function App() {
   );
 
   return (
-    <div className="App">
-      <h1>useReducer Bank Account</h1>
-      <p>
-        Balance:<span>{balance}</span>
-      </p>
-      <p>
-        Loan:<span>{loan}</span>
-      </p>
+    <>
+      <div className="App">
+        <h1>useReducer Bank Account</h1>
+        <p>
+          Balance:<span>{balance}</span>
+        </p>
+        <p>
+          Loan:<span>{loan}</span>
+        </p>
 
-      <p>
-        <button
-          onClick={() => {
-            dispatch({ type: "openAccount" });
-          }}
-          disabled={isActive}
-        >
-          Open account
-        </button>
-      </p>
-      <p>
-        <button
-          onClick={() => {
-            dispatch({ type: "deposit" });
-          }}
-          disabled={!isActive}
-        >
-          Deposit <span>150</span>
-        </button>
-      </p>
-      <p>
-        <button
-          onClick={() => {
-            dispatch({ type: "withdraw" });
-          }}
-          disabled={!isActive}
-        >
-          Withdraw <span>50</span>
-        </button>
-      </p>
-      <p>
-        <button
-          onClick={() => {
-            dispatch({ type: "requestLoan" });
-          }}
-          disabled={(!isActive && loan === 0) || (isActive && loan !== 0)}
-        >
-          Request a loan of <span>5000</span>
-        </button>
-      </p>
-      <p>
-        <button
-          onClick={() => {
-            dispatch({ type: "payLoan" });
-          }}
-          disabled={(!isActive && loan === 0) || (isActive && loan === 0)}
-        >
-          Pay loan
-        </button>
-      </p>
-      <p>
-        <button
-          onClick={() => {
-            dispatch({ type: "closeAccount" });
-          }}
-          disabled={!isActive}
-        >
-          Close account
-        </button>
-      </p>
-    </div>
+        <p>
+          <button
+            onClick={() => {
+              dispatch({ type: "openAccount" });
+            }}
+            disabled={isActive}
+          >
+            Open account
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() => {
+              dispatch({ type: "deposit" });
+            }}
+            disabled={!isActive}
+          >
+            Deposit <span>150</span>
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() => {
+              dispatch({ type: "withdraw" });
+            }}
+            disabled={!isActive}
+          >
+            Withdraw <span>50</span>
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() => {
+              dispatch({ type: "requestLoan" });
+            }}
+            disabled={(!isActive && loan === 0) || (isActive && loan !== 0)}
+          >
+            Request a loan of <span>5000</span>
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() => {
+              dispatch({ type: "payLoan" });
+            }}
+            disabled={(!isActive && loan === 0) || (isActive && loan === 0)}
+          >
+            Pay loan
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() => {
+              dispatch({ type: "closeAccount" });
+            }}
+            disabled={!isActive}
+          >
+            Close account
+          </button>
+        </p>
+      </div>
+      <Mo3bassia />
+    </>
   );
 }
